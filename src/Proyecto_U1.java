9c06895d1fa77cc919e94cc77099dbaf6b5a6b7b
estra_pxwmdkq <leo22nero@yahoo.com>
2026-03-08 09:08:55 -0600

    //Inicial

---
+++ src/Proyecto_U1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+void main() {
+    Ventana ventana = new Ventana();
+    ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ventana.setSize(600,700);
+    ventana.setVisible(true);
+    ventana.setLocationRelativeTo(null);
+    ventana.setResizable(false);
+
+}
